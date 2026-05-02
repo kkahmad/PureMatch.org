afb11c4d540db41305a2d25d63b89be296cbeadd
Khurram Ahmad <khurram.ahmad@riphah.edu.pk>
2026-05-02 11:21:20 +0500

    fix nodemailer

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/submit-consultation', async (req, res) => {
     
     // Send email notification (configure with your email)
     try {
-        const transporter = nodemailer.createTransporter({
+        const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
                 user: process.env.EMAIL_USER || 'your-email@gmail.com',
