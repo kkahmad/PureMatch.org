84f4330ea10314e1499b86179bdd43c5607bd9df
Khurram Ahmad <khurram.ahmad@riphah.edu.pk>
2026-08-05 18:07:22 +0500

    admin panel

--- server.js
+++ server.js
@@ -79,6 +79,10 @@ app.get('/admin/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'admin-login.html'));
 });
 
+app.get('/admin-login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin-login.html'));
+});
+
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
